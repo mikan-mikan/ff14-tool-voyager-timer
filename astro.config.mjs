2923eab2ad0e87d45829415826f89579df5cbb07
mikan-mikan <51882748+mikan-mikan@users.noreply.github.com>
2025-01-31 23:44:29 +0900

    GTM設置、リンクやテキストのスタイル調整、プライバシーポリシーページ作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
-// @ts-check
+import partytown from "@astrojs/partytown";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
