a6d52bdd2527a6dfd7b4eabfde6982827611a157
mikan-mikan <51882748+mikan-mikan@users.noreply.github.com>
2025-02-09 02:01:47 +0900

    GTM設置方法を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-import partytown from "@astrojs/partytown";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
+  rollupOptions: [
+    {
+      output: {
+        entryFileNames: "entry.[hash].mjs",
+        chunkFileNames: "chunks/chunk.[hash].mjs",
+        assetFileNames: "assets/asset.[hash][extname]",
       },
-    }),
+    },
   ],
   integrations: [react()],
 });
