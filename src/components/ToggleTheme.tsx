4c4859e05d847be816f2990f5625cbeb1f7f9a6e
mikan-mikan <51882748+mikan-mikan@users.noreply.github.com>
2025-07-14 03:20:25 +0900

    テーマ切り替えボタン、アニメーションできるように修正

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -9,6 +9,41 @@ const getInitialTheme = () => {
   return "light";
 };
 
+const StyledLabel = styled.label`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const StyledToggleWrap = styled.div`
+  position: relative;
+`;
+
+const StyledInput = styled.input`
+  position: absolute;
+  opacity: 0;
+`;
+
+const StyledSlider = styled.div`
+  display: block;
+  width: 3.5rem;
+  height: 2rem;
+  border-radius: 1rem;
+  background-color: var(--bg-toggle-base);
+`;
+
+const StyledDot = styled.div<{ themeDark: boolean }>`
+  position: absolute;
+  top: 0.25rem;
+  left: 0.25rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  background-color: var(${({ themeDark }) => (themeDark ? "--text-primary" : "--text-button")});
+  transition: transform 0.3s ease-in-out;
+  transform: translateX(${({ themeDark }) => (themeDark ? "1.5rem" : "0")});
+`;
+
 const ToggleTheme: React.FC = () => {
   const [theme, setTheme] = useState<string>(getInitialTheme());
 
@@ -22,41 +57,6 @@ const ToggleTheme: React.FC = () => {
     setTheme((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
-  const StyledLabel = styled.label`
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-  `;
-
-  const StyledToggleWrap = styled.div`
-    position: relative;
-  `;
-
-  const StyledInput = styled.input`
-    position: absolute;
-    opacity: 0;
-  `;
-
-  const StyledSlider = styled.div`
-    display: block;
-    width: 3.5rem;
-    height: 2rem;
-    border-radius: 1rem;
-    background-color: var(--bg-toggle-base);
-  `;
-
-  const StyledDot = styled.div<{ themeDark: boolean }>`
-    position: absolute;
-    top: 0.25rem;
-    left: 0.25rem;
-    width: 1.5rem;
-    height: 1.5rem;
-    border-radius: 50%;
-    // background-color: var(--text-primary);
-    background-color: var(${({ themeDark }) => (themeDark ? "--text-primary" : "--text-button")});
-    transform: translateX(${({ themeDark }) => (themeDark ? "1.5rem" : "0")});
-  `;
-
   return (
     <StyledLabel htmlFor="theme-toggle">
       <StyledToggleWrap>
@@ -66,8 +66,8 @@ const ToggleTheme: React.FC = () => {
           id="theme-toggle"
           checked={theme === "dark"}
         />
-        <StyledSlider></StyledSlider>
-        <StyledDot themeDark={theme === "dark"}></StyledDot>
+        <StyledSlider />
+        <StyledDot themeDark={theme === "dark"} />
       </StyledToggleWrap>
     </StyledLabel>
   );
